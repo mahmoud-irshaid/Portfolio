1e4e6c28b41a4bbbc744122cf87b46ec720995d2
mahmoud-irshaid <mirshaid10@gmail.com>
2023-05-09 11:37:55 +0300

    fix company types

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,6 +1,12 @@
+type company = {
+	logo: string,
+	url: string,
+	name: string
+}
+
 export interface Job {
 	jobTitle: string;
-	company: string;
+	company: company;
 	description: string[];
 	fromDate: string;
 	toDate: string | null;
