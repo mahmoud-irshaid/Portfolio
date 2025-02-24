e0ad5feca29182ac65459aae5b3531c92541b536
mahmoud-irshaid <mirshaid10@gmail.com>
2025-02-24 12:29:28 +0300

    Update Experiences

--- src/app/(user)/about/page.tsx
+++ src/app/(user)/about/page.tsx
@@ -4,11 +4,11 @@ import { Container } from '@components/atoms/Container';
 import { IconDownload } from '@components/atoms/Icons';
 import { Education } from '@components/organisms/Education';
 import { WorkExperience } from '@components/organisms/WorkExperience';
+
 import { Job } from '@root/src/types';
 
 export const metadata = {
-	title:
-		'About Mahmoud Irshaid, Software Engineer in Jordan',
+	title: 'About Mahmoud Irshaid, Software Engineer in Jordan',
 	description:
 		'As a passionate front-end software developer, I create amazing websites and web apps to make the internet a better place.',
 };
@@ -16,11 +16,66 @@ export const metadata = {
 const AboutPage = async () => {
 	const jobs: Job[] = [
 		{
-			company: { name: 'Agents on cloud' },
-			jobTitle: 'Mobile Developer',
+			company: { name: 'Elevatus' },
+			jobTitle: 'Front End Developer',
 			fromDate: 'Jun/2022',
 			toDate: 'May/2023',
-			skills: ['TypeScript', 'React', 'Next.js', 'Redux', 'Rest APIs', 'Jest'],
+			skills: [
+				'TypeScript',
+				'React',
+				'Redux',
+				'Rest APIs',
+				'Jest',
+				'Firebase',
+				'SASS',
+			],
+			description: [
+				'● Designed REST calls using React to streamline user interface performance.',
+				'● Maintained high-level expertise in React state management strategies.',
+				'● Collaborated with cross-functional teams to develop seamless integration of APIs, improving data flow within applications.',
+				'● Developed reusable components to facilitate faster development timelines and improve overall code quality.',
+				'● Boosted application efficiency by incorporating Redux for state management and streamlining component rerendering.',
+				'● Reduced application load times by optimizing React components and utilizing code-splitting techniques.',
+				'● Write and maintain unit tests, integration tests, and end-to-end tests to ensure code reliability.',
+			],
+		},
+		{
+			company: { name: 'Coderz For Software And Training' },
+			jobTitle: 'Front End Developer',
+			fromDate: 'Aug/2023',
+			toDate: 'Sept/2024',
+			skills: [
+				'TypeScript',
+				'React',
+				'Next.js',
+				'Redux',
+				'Rest APIs',
+				'GraphQL',
+				'Leaflet',
+			],
+			description: [
+				'● Reviewed code and debugged problems to validate structures, assess security and verify browser, device and operating system compatibility.',
+				'● Advocated for well-tested and documented, high quality code.',
+				'● Designed GraphQL APIs calls using React to streamline user interface performance.',
+				'● Updated old code bases to modern development standards, improving functionality.',
+				'● Designed and developed interactive, real-time mapping applications for live tracking purposes.',
+				'● Optimized application performance for smooth rendering and data synchronization in live tracking scenarios.',
+			],
+		},
+		{
+			company: { name: 'Agents on cloud' },
+			jobTitle: 'Front End Developer',
+			fromDate: 'Jun/2022',
+			toDate: 'Aug/2023',
+			skills: [
+				'React',
+				'React Native',
+				'Expo',
+				'Redux',
+				'Rest APIs',
+				'Nx',
+				'Git',
+			],
 			description: [
 				'● Developed front-end user interface using React Native and JavaScript for iOS and Android mobile applications.',
 				'● Collaborated with other developers to implement new features in a way that was consistent with existing codebase conventions.',
@@ -29,6 +84,20 @@ const AboutPage = async () => {
 				'● Participated in design discussions regarding application architecture, feature implementation, or any other relevant topic as required.',
 				'● Worked with Nx as a mono repo in managing and developing Expo applications within a workspace.',
 				'● Collaborated with designers on UI/UX design decisions and created mockups as needed.',
+			],
+		},
+		{
+			company: { name: 'Freelance' },
+			jobTitle: 'Full Stack Developer',
+			fromDate: 'March/2022',
+			toDate: 'June/2022',
+			skills: ['HTML', 'ReCssact', 'JavaScript', 'AJAX', 'Rest APIs'],
+			description: [
+				'● Planned website development, converting mockups into usable  web presence with HTML, Css, JavaScript, AJAX, and JSON coding.',
+				'● Skilled at working independently and collaboratively in a team environment.',
+				'● Built APIs and data clients to consume APIs.',
+				'● Designed and implemented RESTful APIs for mobile apps with JSON data exchange.',
+				'● Designed and updated layouts to meet usability and performance requirements.',
 			],
 		},
 	];
